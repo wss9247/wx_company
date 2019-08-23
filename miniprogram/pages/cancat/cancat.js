ab5dc420e481fd6ee5b06a55b8bbebfc0777f961
wss9247 <245273360@qq.com>
2019-08-23 20:47:44 +0800

    添加首页跳转菜单

--- miniprogram/pages/cancat/cancat.js
+++ miniprogram/pages/cancat/cancat.js
@@ -1,38 +1,18 @@
-// pages/index/index.js
-
+// pages/cancat/cancat.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    images: [
-      'https://img.yzcdn.cn/vant/apple-1.jpg',
-      'https://img.yzcdn.cn/vant/apple-2.jpg'
-    ],
-   urls:[
-     { url: "/pages/cancat/cancat", name:"教育服务"},
-     { url: "/pages/cancat/cancat", name:"教育研究"},
-     { url: "/pages/cancat/cancat", name:"技能竞赛"},
-     { url: "/pages/cancat/cancat", name:"示范案例"},
-     { url: "/pages/cancat/cancat", name:"团队管理"},
-     { url: "/pages/cancat/cancat", name:"合作动态"},
-     { url: "/pages/cancat/cancat", name:"学员风采"},
-     { url: "/pages/cancat/cancat", name:"联系我们"}
-   ]
+
   },
-  jumpTo(event){
-    var url = event.target.dataset.url;
-    console.log(url)
-    wx.navigateTo({
-      url: url
-    })
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-   
+
   },
 
   /**
